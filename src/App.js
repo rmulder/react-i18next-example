5cf60ab73a15fd6578666a46d12e2e4733a051b6
Robert Matthew Mulder <rmmulder@gmail.com>
2021-05-06 20:48:43 +0800

    Save work in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+/**
+ * Import i18next
+ */
+import i18next from 'i18next';
+/** Import the config that just created */
+import './i18n.config';
 
 const App = () => {
+  /**
+   * Use useTranslation hooks
+   */
+  const { t } = useTranslation();
   const [selectedLanguage, setSelectedLanguage] = useState('en');
   const handleLanguageSelect = (event) => {
     setSelectedLanguage(event.target.value);
+    /**
+     * Set the language to either en or zh
+     */
+    i18next.changeLanguage(event.target.value);
   }
 
   return (
@@ -18,7 +33,10 @@ const App = () => {
       </label>
       <p>Your selected language is: {selectedLanguage} </p>
       <p>Your have switch language for 0 times</p>
-      <p>Welcome to the world of wonder</p>
+      {/**
+        * Use the key in the language file to get the translated text
+        */}
+      <p>{t('welcome')}</p>
     </div>
   );
 }
---
+++ src/i18n.config.js
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './en.json';
+import zh from './zh.json';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {
+        translation: en, // Add translation for English
+      },
+      zh: {
+        translation: zh, // Add translation for Chinese
+      },
+    },
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false // No need to escape for react
+    }
+  });
+  
